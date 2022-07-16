5a037544ba234275b479b624827e91a34cc47f8d
Jeni Rainer <jenirainer@icloud.com>
2022-07-16 16:36:06 +0200

    adding remove methods and other tests

--- src/main/java/doubleLinkedList/MyLinkedList.java
+++ src/main/java/doubleLinkedList/MyLinkedList.java
@@ -1,11 +1,11 @@
 package doubleLinkedList;
 
 public class MyLinkedList<E> {
-	private Node<E> head;
-	private Node<E> tail;
+	private final Node<E> head;
+	private final Node<E> tail;
 	private int size;
 
-	public MyLinkedList(){
+	public MyLinkedList() {
 		head = new Node<>(null);
 		tail = new Node<>(null);
 		head.setNext(tail);
@@ -15,9 +15,10 @@ public class MyLinkedList<E> {
 
 	/**
 	 * This adds a node to the end of the list.
+	 *
 	 * @param nodeValue - the value to add to the list.
 	 */
-	public Node<E> addNode(E nodeValue){
+	public Node<E> addNode(E nodeValue) {
 		Node<E> lastBeforeAdd = tail.getPrevious();
 		Node<E> nodeToAdd = new Node<>(nodeValue);
 		nodeToAdd.setPrevious(lastBeforeAdd);
@@ -28,22 +29,43 @@ public class MyLinkedList<E> {
 		return nodeToAdd;
 	}
 
+	/**
+	 * clean this up for readability when you don't have COVID-19 brain fog.
+	 *
+	 * @param nodeValue the value to set on the node to insert.
+	 * @param index     where to insert the value in the list.
+	 * @return Node of type E
+	 */
 	public Node<E> addNodeAtIndex(E nodeValue, int index) {
+		Node<E> nodeToAdd = new Node<>(nodeValue);
 		if (index < 0 || index > size) {
 			throw new IndexOutOfBoundsException();
+		} else if (size == 0) {
+			nodeToAdd = addNode(nodeValue);
+		} else {
+			Node<E> nodePreviouslyAtIndex = getNode(index);
+			Node<E> prevOfPrevious = nodePreviouslyAtIndex.getPrevious();
+			nodeToAdd.setPrevious(prevOfPrevious);
+			nodeToAdd.setNext(nodePreviouslyAtIndex);
+			prevOfPrevious.setNext(nodeToAdd);
+			nodePreviouslyAtIndex.setPrevious(nodeToAdd);
+			size++;
 		}
-		if (size == 0) {
-			return addNode(nodeValue);
-		}
-		Node<E> nodeToAdd = new Node<>(nodeValue);
-		Node<E> nodePreviouslyAtIndex = getNode(index); // this will be the next
-		Node<E> prevOfPrevious = nodePreviouslyAtIndex.getPrevious();
-		nodeToAdd.setPrevious(prevOfPrevious);
-		nodeToAdd.setNext(nodePreviouslyAtIndex);
-		prevOfPrevious.setNext(nodeToAdd);
-		nodePreviouslyAtIndex.setPrevious(nodeToAdd);
-		size++;
 		return nodeToAdd;
+	}
+
+	/**
+	 *
+	 * @param toRemove the element to remove from the list
+	 * @throws ItemNotFoundException when the item to remove is not found.
+	 */
+	public void removeElement(E toRemove) throws ItemNotFoundException {
+		Node<E> nodeToRemove = find(toRemove);
+		Node<E> nodeBefore = nodeToRemove.getPrevious();
+		Node<E> nodeAfter = nodeToRemove.getNext();
+		nodeBefore.setNext(nodeAfter);
+		nodeAfter.setPrevious(nodeBefore);
+		size--;
 	}
 
 	/**
@@ -51,17 +73,36 @@ public class MyLinkedList<E> {
 	 * whether we should search from head or tail; but since we don't,
 	 * it's almost impossible to figure out the mid-value.
 	 * Just search from head for now.
-	 * @param value
+	 *
+	 * @param value the element to find in the list
 	 * @return the node in the list which holds that value.
-	 * @throws ItemNotFoundException
+	 * @throws ItemNotFoundException if the item is not found in the list an exception will be thrown.
 	 */
 	public Node<E> find(E value) throws ItemNotFoundException {
-		throw new ItemNotFoundException("not yet implemented");
+		Node<E> found = null;
+		Node<E> currentNode = head;
+		for (int i = 0; i < this.size; i++){
+			currentNode = currentNode.getNext();
+			if (currentNode.getData().equals(value)) {
+				found = currentNode;
+			}
+		}
+		if (found == null) {
+			throw new ItemNotFoundException(value.toString() + " was not found.");
+		} else {
+			return found;
+		}
 	}
 
-	private Node<E> getNode(int index){
-		Node<E> nodeAtIndex = head;
-		for (int i = 0; i < index; i++){
+	/**
+	 * This is protected so that it can be used in testing.
+	 *
+	 * @param index which node to pull out of the list
+	 * @return Node of type E
+	 */
+	protected Node<E> getNode(int index) {
+		var nodeAtIndex = head;
+		for (int i = 0; i < index; i++) {
 			nodeAtIndex = nodeAtIndex.getNext();
 		}
 		return nodeAtIndex;
@@ -76,27 +117,19 @@ public class MyLinkedList<E> {
 		return size;
 	}
 
-	public void setSize(int size) {
-		this.size = size;
-	}
-
 	public Node<E> getHead() {
 		return head;
-	}
-
-	public void setHead(Node<E> head) {
-		this.head = head;
 	}
 
 	public Node<E> getTail() {
 		return tail;
 	}
 
-	public void setTail(Node<E> tail) {
-		this.tail = tail;
-	}
 
-	public void prettyPrintList(){
+	/**
+	 * Used for debugging purposes.
+	 */
+	public void prettyPrintList() {
 		System.out.println("Printing nodes from this list:  ");
 		Node<E> nextNode = head.getNext();
 		while (!nextNode.equals(tail)) {
--- src/test/java/doubleLinkedList/MyLinkedListTest.java
+++ src/test/java/doubleLinkedList/MyLinkedListTest.java
@@ -12,13 +12,13 @@ class MyLinkedListTest {
 	private MyLinkedList<Integer> testList;
 
 	@Test
-	public void testCreationOfList(){
+	public void testCreationOfList() {
 		MyLinkedList<Integer> actual = new MyLinkedList<>();
 		assertEquals(0, actual.getSize());
 	}
 
 	@BeforeEach
-	public void setTestList(){
+	public void setTestList() {
 		testList = new MyLinkedList<>();
 		testList.addNode(1);
 		testList.addNode(2);
@@ -26,7 +26,7 @@ class MyLinkedListTest {
 	}
 
 	@Test
-	public void testBuildSmallishList(){
+	public void testBuildSmallishList() {
 		assertEquals(3, testList.getSize());
 		Node<Integer> theOneAfterHead = testList.getHead().getNext();
 		assertEquals(Integer.valueOf(1), theOneAfterHead.getData());
@@ -37,7 +37,7 @@ class MyLinkedListTest {
 	}
 
 	@Test
-	public void testAddNodeAtIndex(){
+	public void testAddNodeAtIndex() {
 		Node<Integer> actual = testList.addNodeAtIndex(5, 2);
 		assertEquals(4, testList.getSize());
 		assertEquals(5, testList.get(2));
@@ -52,15 +52,15 @@ class MyLinkedListTest {
 			"5,7",
 			"4,-5"
 	})
-	public void testAddNodeAtInvalidIndex(int nodeVal, int index){
-		IndexOutOfBoundsException thrown = assertThrows(IndexOutOfBoundsException.class, () -> {
-			testList.addNodeAtIndex(nodeVal, index);
-		});
+	public void testAddNodeAtInvalidIndex(int nodeVal, int index) {
+		IndexOutOfBoundsException thrown = assertThrows(IndexOutOfBoundsException.class, () ->
+				testList.addNodeAtIndex(nodeVal, index)
+		);
 		assertNotNull(thrown);
 	}
 
 	@Test
-	public void testAddNodeAtIndexInEmptyList(){
+	public void testAddNodeAtIndexInEmptyList() {
 		MyLinkedList<String> empty = new MyLinkedList<>();
 		Node<String> inserted = empty.addNodeAtIndex("A", 0);
 		assertEquals(1, empty.getSize());
@@ -68,4 +68,40 @@ class MyLinkedListTest {
 		assertEquals(inserted, empty.getHead().getNext());
 	}
 
+	@Test
+	public void testFindSimple() throws ItemNotFoundException {
+		Node<Integer> one = testList.getNode(1);
+		Node<Integer> actual = testList.find(1);
+		assertEquals(one, actual);
+	}
+
+	@Test
+	public void testFindWithoutElementThrowsException() {
+		ItemNotFoundException thrown = assertThrows(ItemNotFoundException.class, () ->
+				testList.find(8)
+		);
+		assertEquals("8 was not found.", thrown.getMessage());
+	}
+
+	@Test
+	public void testSimpleRemove() throws ItemNotFoundException {
+		testList.removeElement(2);
+		testList.prettyPrintList();
+		assertEquals(2, testList.getSize());
+		ItemNotFoundException thrown = assertThrows(ItemNotFoundException.class, () -> testList.find(2));
+		assertEquals("2 was not found.", thrown.getMessage());
+		Node<Integer> oneNode = testList.getNode(1);
+		Node<Integer> threeNode = testList.getNode(2);
+		assertEquals(oneNode, testList.getHead().getNext());
+		assertEquals(oneNode, threeNode.getPrevious());
+		assertEquals(threeNode, oneNode.getNext());
+		assertEquals(testList.getTail(), threeNode.getNext());
+	}
+
+	@Test
+	public void testRemoveElementWhichIsNotInList() {
+		ItemNotFoundException thrown = assertThrows(ItemNotFoundException.class, () ->
+				testList.removeElement(99));
+		assertEquals("99 was not found.", thrown.getMessage());
+	}
 }
